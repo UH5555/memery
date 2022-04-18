092783d0edf0f39676579ea39e6d7dd47e892f93
UH5555 <zhuhao162@163.com>
2022-04-18 19:44:37 +0800

    0418\内存（一）

---
+++ test.c
@@ -0,0 +1,95 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h> 
+#include <assert.h>
+
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[5] = { 0 };
+//	strcpy(arr2, arr1);
+//	return 0;
+//}
+
+struct S
+{
+	char name[20];
+    int age;
+};
+
+void* my_memcpy(void* dest, const void* src, size_t num)
+{
+	void* ret = dest;
+	assert(dest != NULL);
+	assert(src != NULL);
+	while (num--)
+	{
+		*(char*)dest = *(char*)src;
+		++(char*)dest;
+		++(char*)src;
+	}
+	return ret;
+}
+
+//C语言标准：
+//memcpy  只要处理 不重叠的内存拷贝就可以
+//memmove 处理重叠内存的拷贝
+void* my_memmove(void* dest, void* const src, size_t num)
+{
+	void* ret = dest;
+	assert(dest != NULL);
+	assert(src != NULL);
+	size_t a = num;
+	if ((char*)dest < (char*)src)
+	{
+		char* a = dest + (char*)a - 1;
+		char* b = src + (char*)a - 1;
+		while (num--)
+		{
+			*a = *b;
+			--(char*)dest;
+			--(char*)src;
+		}
+	}
+	else
+	{
+		while (num--)
+		{
+			*(char*)dest = *(char*)src;
+			++(char*)dest;
+			++(char*)src;
+		}
+	}
+	return ret;
+}
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+	//my_memcpy(arr + 2, arr, 20);
+	//memmove(arr + 2, arr, 20);//处理内存重叠的情况
+	my_memmove(arr + 2, arr, 20);
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+//int main()
+//{
+//	struct S arr3[] = { {"张三",20},{"李四",30} };
+//	struct S arr4[3] = { 0 };
+//	//memcpy(arr4, arr3, sizeof(arr3));
+//	my_memcpy(arr4, arr3, sizeof(arr3));
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[5] = { 0 };
+//	memcpy(arr2, arr1, sizeof(arr1));
+//	return 0;
+//}
