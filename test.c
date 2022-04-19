0cccde1496450c0891d8b950a31f0ba487d34cb3
UH5555 <zhuhao162@163.com>
2022-04-19 20:46:05 +0800

    0419\内存（二）

--- test.c
+++ test.c
@@ -11,71 +11,67 @@
 //	return 0;
 //}
 
-struct S
-{
-	char name[20];
-    int age;
-};
+//struct S
+//{
+//	char name[20];
+//    int age;
+//};
+//
+//void* my_memcpy(void* dest, const void* src, size_t num)
+//{
+//	void* ret = dest;
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	while (num--)
+//	{
+//		*(char*)dest = *(char*)src;
+//		++(char*)dest;
+//		++(char*)src;
+//	}
+//	return ret;
+//}
 
-void* my_memcpy(void* dest, const void* src, size_t num)
-{
-	void* ret = dest;
-	assert(dest != NULL);
-	assert(src != NULL);
-	while (num--)
-	{
-		*(char*)dest = *(char*)src;
-		++(char*)dest;
-		++(char*)src;
-	}
-	return ret;
-}
-
-//C语言标准：
-//memcpy  只要处理 不重叠的内存拷贝就可以
-//memmove 处理重叠内存的拷贝
-void* my_memmove(void* dest, void* const src, size_t num)
-{
-	void* ret = dest;
-	assert(dest != NULL);
-	assert(src != NULL);
-	size_t a = num;
-	if ((char*)dest < (char*)src)
-	{
-		char* a = dest + (char*)a - 1;
-		char* b = src + (char*)a - 1;
-		while (num--)
-		{
-			*a = *b;
-			--(char*)dest;
-			--(char*)src;
-		}
-	}
-	else
-	{
-		while (num--)
-		{
-			*(char*)dest = *(char*)src;
-			++(char*)dest;
-			++(char*)src;
-		}
-	}
-	return ret;
-}
-
-int main()
-{
-	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-	int i = 0;
-	//my_memcpy(arr + 2, arr, 20);
-	//memmove(arr + 2, arr, 20);//处理内存重叠的情况
-	my_memmove(arr + 2, arr, 20);
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	return 0;
-}
+////C语言标准：
+////memcpy  只要处理 不重叠的内存拷贝就可以
+////memmove 处理重叠内存的拷贝
+//void* my_memmove(void* dest, const void*  src, size_t num)
+//{
+//	void* ret = dest;
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	size_t a = num;
+//	if (dest > src)
+//	{
+//		while (num--)
+//		{
+//			*((char*)dest + num) = *((char*)src + num);
+//		}
+//	}
+//	else
+//	{
+//		while (num--)
+//		{
+//			*(char*)dest = *(char*)src;
+//			++(char*)dest;
+//			++(char*)src;
+//		}
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	//my_memcpy(arr + 2, arr, 20);
+//	//memmove(arr + 2, arr, 20);//处理内存重叠的情况
+//	my_memmove(arr + 2, arr, 20);
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
 
 //int main()
 //{
@@ -93,3 +89,21 @@ int main()
 //	memcpy(arr2, arr1, sizeof(arr1));
 //	return 0;
 //}
+
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[] = { 1,2,5,4,3 };
+//	int ret = memcmp(arr1, arr2, 9);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//memset - 内存设置
+
+int main()
+{
+	char arr[10] = "";
+	memset(arr, '#', 10); //10的单位为字节。
+	return 0;
+}
